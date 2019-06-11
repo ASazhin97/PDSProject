7917dca47894a27347776884c5c6bfb02e0e873f
PaulSMc <paul@juniorgolfhub.com>
2019-06-11 10:36:52 -0400

    changed path in xlWork to reference 2017-18 data

--- directory combination.py
+++ directory combination.py
@@ -1,9 +1,9 @@
-import shutil
+
 import xlrd
 
-PDS = xlrd.open_workbook("All Hat and PDS tests.xlsx")
+PDS = xlrd.open_workbook("2017-2018 query.xlsx")
 sheet = PDS.sheet_by_index(0)
-paths = [sheet.cell_value(col, 8) for col in range(1, 133)]
+paths = [sheet.cell_value(col, 7) for col in range(1, 85)]
 print(paths)
 
 
